e5409c8634cfbabe7cc67dec03ce4bdc5e902ac6
Nazarii <72383249+GIT-Nazariy1213@users.noreply.github.com>
2024-12-13 00:57:52 +0200

    Update lesson2_task11.erl

--- lesson2_task11.erl
+++ lesson2_task11.erl
@@ -1,10 +1,11 @@
 -module(lesson2_task11).
 -export([encode_modified/1]).
 
-encode_modified(List) ->
-    lists:map(fun(X) -> 
-        case length(X) of 
-            1 -> hd(X); 
-            _ -> {length(X), hd(X)} 
-        end 
-    end, lesson2_task09:pack(List)).
+encode_modified(List) -> 
+    encode_packed(lesson2_task09:pack(List)).
+
+encode_packed([]) -> [];
+encode_packed([[H] | Rest]) -> 
+    [H | encode_packed(Rest)];
+encode_packed([[H | T] | Rest]) -> 
+    [{length([H | T]), H} | encode_packed(Rest)].
